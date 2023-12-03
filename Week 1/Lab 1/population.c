26d4f4bfc0feccd3897d4c2776fc00ce979bc47f
Javier Salinas <javitrrox@gmail.com>
2023-12-03 13:58:35 -0300

    Initial Commit

---
+++ Week 1/Lab 1/population.c
@@ -0,0 +1,29 @@
+#include <cs50.h>
+#include <stdio.h>
+
+int main(void)
+{
+    int start;
+    do
+    {
+        start = get_int("Start size?: ");
+    }
+    while (start < 9);
+
+    int end;
+    do
+    {
+        end = get_int("End size: ");
+    }
+    while (end < start);
+
+    int years = 0;
+
+    while (start < end)
+    {
+        start = start + start / 3 - start / 4;
+        years++;
+    }
+
+    printf("Years: %i\n", years);
+}
