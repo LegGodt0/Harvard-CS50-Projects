367ce686d97d6149c0374367a2766d9305eedcc1
Javier Salinas <javitrrox@gmail.com>
2024-04-19 21:00:18 -0400

    Problem set mario update 3

---
+++ Week 1/Problem set 1/mario-less/mario.c
@@ -0,0 +1,41 @@
+#include <cs50.h>
+#include <stdio.h>
+
+int get_size(void);
+void print_grid(int size);
+
+int main(void)
+{
+    // Get size of grid
+    int n = get_size();
+
+    // Print grid of bricks
+    print_grid(n);
+}
+
+int get_size(void)
+{
+    int n;
+    do
+    {
+        n = get_int("Size: ");
+    }
+    while (n < 1 || n > 8);
+    return n;
+}
+
+void print_grid(int size)
+{
+    for (int i = 0; i < size; i++)
+    {
+        for (int s = size - 1; s > i; s--)
+        {
+            printf(" ");
+        }
+        for (int j = 0; j <= i; j++)
+        {
+            printf("#");
+        }
+        printf("\n");
+    }
+}
